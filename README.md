6f5883248f0a529f1f253f38d2cb5a2c6c7c8838
rrobert45 <rrobert45@gmail.com>
2023-02-11 21:31:51 -0500

    updates

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# Egg_App
- New Egg App
+Egg Incubator
+
+This Python program uses a Raspberry Pi 4B, an AHT20 temperature and humidity sensor, and a low signal relay to control the temperature and humidity in an egg incubator. The program includes an automatic egg turning feature, a web interface for monitoring the temperature, humidity, and incubation progress, and a MongoDB database for logging incubator performance data.
+
+To use this program, follow these steps:
+
+1. Install the required hardware, including the Raspberry Pi 4B, the AHT20 sensor, and the low signal relay.
+
+2. Install the necessary software packages by running the following command in your terminal:
+
+   pip install -r requirements.txt
+
+3. Modify the `config.json` file to specify the start date of the incubation and the desired humidity ranges for different days of the incubation period.
+
+4. Connect the low signal relay to the appropriate GPIO pins on the Raspberry Pi 4B. Update the `app.py` file to reflect the correct GPIO pin numbers for temperature control, humidity control, and egg turning.
+
+5. Start the program by running the following command in your terminal:
+
+   python app.py
+
+6. View the egg incubator web interface by navigating to `http://<raspberrypi_IP_address>:5000` in your web browser.
+
+7. Use the web interface to monitor the temperature, humidity, and incubation progress of the egg incubator. Use the egg turning feature to trigger egg turning manually or automatically.
+
+Note: The `RPi.GPIO` library used for GPIO control requires root access. Therefore, you will need to run the program with `sudo` privileges or by adding your user to the `gpio` group using the `adduser` command.
+
+For more information, please consult the documentation for the Raspberry Pi 4B, the AHT20 sensor, the low signal relay, the Python libraries used in this program, and the Flask web framework.
---
+++ app.py
@@ -0,0 +1,138 @@
+import time
+import board
+import adafruit_ahtx0
+import RPi.GPIO as GPIO
+import json
+from datetime import datetime, timedelta
+from pymongo import MongoClient
+from flask import Flask, render_template, request
+
+# Initialize GPIO pins for temperature and humidity control
+GPIO.setmode(GPIO.BCM)
+temp_pin = 23  # Change to appropriate GPIO pin for temperature control relay
+humid_pin = 24  # Change to appropriate GPIO pin for humidity control relay
+GPIO.setup(temp_pin, GPIO.OUT)
+GPIO.setup(humid_pin, GPIO.OUT)
+
+# Initialize AHT20 temperature and humidity sensor
+i2c = board.I2C()
+sensor = adafruit_ahtx0.AHTx0(i2c)
+
+# Initialize Flask web application
+app = Flask(__name__)
+
+# Initialize MongoDB database
+client = MongoClient('mongodb://localhost:27017/')
+db = client['incubator']
+
+# Load configuration from JSON file
+with open('config.json') as f:
+    config = json.load(f)
+
+# Retrieve start date and humidity ranges from configuration
+start_date = datetime.strptime(config['start_date'], '%Y-%m-%d').date()
+humidity_ranges = config['humidity_ranges']
+
+# Define temperature and humidity thresholds
+temp_low = 36.7  # Celsius
+temp_high = 37.8  # Celsius
+humid_low = humidity_ranges[0][0]  # Percent
+humid_high = humidity_ranges[0][1]  # Percent
+
+# Initialize variables for egg turning
+turn_pin = 25  # Change to appropriate GPIO pin for egg turning relay
+GPIO.setup(turn_pin, GPIO.OUT)
+turn_interval = 2 * 60 * 60  # 2 hours in seconds
+turn_duration = 2 * 60  # 2 minutes in seconds
+last_turn = time.time()
+
+# Define functions for temperature and humidity control
+def set_temp(value):
+    if value:
+        GPIO.output(temp_pin, GPIO.LOW)
+    else:
+        GPIO.output(temp_pin, GPIO.HIGH)
+
+def set_humid(value):
+    if value:
+        GPIO.output(humid_pin, GPIO.LOW)
+    else:
+        GPIO.output(humid_pin, GPIO.HIGH)
+
+# Define function for egg turning
+def turn_eggs():
+    GPIO.output(turn_pin, GPIO.LOW)
+    time.sleep(turn_duration)
+    GPIO.output(turn_pin, GPIO.HIGH)
+
+# Define function for calculating incubation day
+def calculate_day():
+    delta = datetime.now().date() - start_date
+    return delta.days + 1
+
+# Define function for calculating humidity based on incubation day
+def calculate_humidity(day):
+    if day >= 18:
+        return 70
+    else:
+        for range in humidity_ranges:
+            if day <= range[2]:
+                return range[1]
+        return 0
+
+# Define function for logging data to MongoDB
+def log_data(temperature, humidity, day):
+    data = {'timestamp': datetime.now(), 'temperature': temperature, 'humidity': humidity, 'day': day}
+    db.incubator.insert_one(data)
+
+# Define Flask route for displaying web interface
+@app.route('/')
+def index():
+    temperature = sensor.temperature
+    humidity = sensor.relative_humidity
+    day = calculate_day()
+    hatch_day = start_date + timedelta(days=21)
+    progress = day / 21 * 100
+    return render_template('index.html', temperature=temperature, humidity=humidity, day=day, hatch_day=hatch_day, progress=progress)
+
+# Define Flask route for triggering egg turning
+@app.route('/turn')
+def turn():
+    global last_turn
+    if time.time() - last_turn >= turn_interval:
+        last_turn = time.time()
+        turn_eggs()
+        return 'Egg turning triggered'
+    else:
+        return 'Egg turning already triggered within the last 2 hours'
+
+if __name__ == '__main__':
+    app.run()
+
+# Main program loop
+while True:
+    # Read temperature and humidity from sensor
+    temperature = sensor.temperature
+    humidity = sensor.relative_humidity
+
+    # Control temperature based on user-defined thresholds
+    if temperature < temp_low:
+        set_temp(True)
+    elif temperature > temp_high:
+        set_temp(False)
+
+    # Control humidity based on calculated humidity ranges
+    day = calculate_day()
+    humid_target = calculate_humidity(day)
+    if humid_target == 0:
+        set_humid(False)
+    elif humidity < humid_target - 2:
+        set_humid(True)
+    elif humidity > humid_target + 2:
+        set_humid(False)
+
+    # Log data to MongoDB
+    log_data(temperature, humidity, day)
+
+    # Wait 10 seconds before repeating loop
+    time.sleep(10)
